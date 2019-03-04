5dbf6f57fdd21c753f453f4e6f48170f0072e6c0
Bo <Bo.Yu@Intelsat.com>
2019-03-04 18:04:23 -0500

    shell script to run .py file

---
+++ call_pso.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+#This script is to execute ParticleSwarm.py
+
+pythonFile="/ParticleSwarm.py"
+dirPath="$(pwd)"
+filePath=$dirPath$pythonFile
+
+/c/Users/yubo/AppData/Local/Programs/Python/Python36-32/Python.exe "$filePath"
+
